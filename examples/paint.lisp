6acfb84d280a6ac0bda3f837c1b93f92eb58ea5d
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-17 21:27:30 -0300

    Remove trailing whitespace

--- examples/paint.lisp
+++ examples/paint.lisp
@@ -20,7 +20,7 @@
   ;; We don't want to move the cursor when we paint.
   (charms:with-restored-cursor charms:*standard-window*
     (charms:write-char-at-cursor
-     charms:*standard-window* 
+     charms:*standard-window*
      (if (char/= #\Space (charms:char-at-cursor charms:*standard-window*))
          #\Space
          #\*))))
@@ -33,7 +33,7 @@
     (charms:disable-echoing)
     (charms:enable-raw-input :interpret-control-characters t)
     (charms:enable-non-blocking-mode charms:*standard-window*)
-    
+
     (loop :named driver-loop
           :with x := 0                  ; Cursor X coordinate
           :with y := 0                  ; Cursor Y coordinate
@@ -42,7 +42,7 @@
           :do (progn
                 ;; Refresh the window
                 (charms:refresh-window charms:*standard-window*)
-                
+
                 ;; Process input
                 (case c
                   ((nil) nil)
@@ -52,12 +52,12 @@
                   ((#\d) (incf x))
                   ((#\Space) (paint))
                   ((#\q #\Q) (return-from driver-loop)))
-                
+
                 ;; Normalize the cursor coordinates
                 (multiple-value-bind (width height)
                     (charms:window-dimensions charms:*standard-window*)
                   (setf x (mod x width)
                         y (mod y height)))
-                
+
                 ;; Move the cursor to the new location
                 (charms:move-cursor charms:*standard-window* x y)))))
